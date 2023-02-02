a5c618f6882514448d4cba74178f30486d8d382f
FemiDeji <adeniyioluwafemiayodeji@gmail.com>
2023-02-02 15:37:14 -0800

    Initial commit

---
+++ src/SimpleCalc.java
@@ -0,0 +1,9 @@
+public class SimpleCalc {
+    public static void main(String[] args){
+        int a, b;
+        a=6;
+        b=3;
+        int sum = a+b;
+                System.out.println(sum);
+    }
+}
